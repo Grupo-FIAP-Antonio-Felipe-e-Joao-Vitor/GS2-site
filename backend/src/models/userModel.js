37a47a484dc565fb979ea362926ab6e134497a66
Antonio <antoniojacintoa@gmail.com>
2025-11-10 08:31:16 -0300

    fix(backend): adicionando validacao de campo

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -3,11 +3,32 @@ import mongoose from "mongoose";
 // Criando Schema de usuarios
 const userSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    nome: { type: String },
-    empresa: { type: String },
-    atuacao: { type: String },
-    email: { type: String },
-    senha: { type: String }
+    nome: { 
+        type: String,
+        required: true
+    },
+    empresa: { 
+        type: String,
+        required: true,
+        enum: {
+            values: ["Fiap", "Google", "Mercado livre"]
+        } 
+    },
+    atuacao: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["TI", "Marketing", "Vendas", "RH", "Financeiro"]
+        }
+    },
+    email: { 
+        type: String,
+        required: true
+    },
+    senha: { 
+        type: String,
+        required: true
+    }
 }, { timestamps: true });
 
 // Criando model de usuarios
