57936a47784e900544ace1366374af5fb9c63fdd
Antonio <antoniojacintoa@gmail.com>
2025-11-10 08:49:19 -0300

    feat(frontend): criando componentes basicos e rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Cadastro from "./routes/Cadastro"
+import Home from "./routes/Home"
+import Login from "./routes/Login"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+
+      <Header />
+
+      <main>
+
+        <Routes>
+
+          <Route path="/" element={<Home />}/>
+          <Route path="/cadastro" element={<Cadastro />}/>
+          <Route path="/login" element={<Login />}/>
+
+        </Routes>
+
+      </main>
+
+      <Footer />
+
+    </BrowserRouter>
   )
 }
 
