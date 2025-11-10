546e4ea7b7da353b847f6b377e9fed3be82347ce
Antonio <antoniojacintoa@gmail.com>
2025-11-10 09:15:39 -0300

    feat(frontend): criando header e login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full p-2 text-white bg-slate-800">
+      <nav className="flex justify-around">
+        <Link to="/" className="uppercase text-lg">Home</Link>
 
-export default Header
+        <ul className="flex space-x-8">
+          <li className="list-none uppercase text-lg">
+            <Link to="/form">Health form</Link>
+          </li>
+          <li className="list-none uppercase text-lg">
+            <Link
+                className="text-white px-5 border-2 border-white rounded-lg bg-transparent cursor-pointer hover:bg-white hover:text-black transition-all duration-300" 
+                to="/login"
+            >
+                Login
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import imagem from "../assets/imagem-login.jpg"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    
+    <div className='flex w-full min-h-screen bg-slate-900'>
+        
+        <div className="hidden md:block md:w-1/2">
+            <img
+                className="w-full h-full object-cover object-left" 
+                src={imagem} 
+                alt="imagem" 
+            />
+        </div>
+        
+        <form
+            className="w-full md:w-1/2 flex flex-col justify-center items-center"
+        >
+
+            <div className="flex flex-col gap-1">
+                <label
+                    className="text-lg text-white"
+                >
+                    E-mail
+                </label>
+            </div>
+
+        </form>
+    </div>
   )
 }
 
