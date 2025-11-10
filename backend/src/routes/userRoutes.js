13d3eefe54d89eaf44f7a1babf07b8821e404ef0
Antonio <antoniojacintoa@gmail.com>
2025-11-10 08:31:59 -0300

    feat(backend): criando rota get que mostra todos os usuarios

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,6 +3,7 @@ import UserController from "../controllers/UserController.js";
 
 const route = express.Router();
 
+route.get("/usuarios", UserController.listar_usuarios);
 route.post("/register", UserController.registro);
 route.post("/login", UserController.login);
 
