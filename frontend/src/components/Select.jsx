d9b83862b09aee1822bca92355a25a554fa8f2ee
Antonio <antoniojacintoa@gmail.com>
2025-11-10 17:54:10 -0300

    feat(frontend): criando componente select, um input dropdown

---
+++ frontend/src/components/Select.jsx
@@ -0,0 +1,25 @@
+const Select = ({ label, options, placeholder, register, name, width = "w-full" }) => {
+    return (
+        <div className={`flex flex-col mb-5 ${width}`}>
+            <label
+                className="text-2xl font-bold text-white"
+            >
+                {label}
+            </label>
+            <select
+                className="bg-gray-300 rounded-sm text-lg cursor-pointer text-black px-2 py-[3px]"
+                defaultValue=""
+                {...(register ? register(name, { required: true }) : {})}
+            >
+                <option value="" disabled hidden>
+                    {placeholder}
+                </option>
+                {options.map((o) => (
+                    <option value={o.value}>{o.label}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default Select
