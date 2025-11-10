42e326837391303b06f908526d33e233959436e4
Antonio <antoniojacintoa@gmail.com>
2025-11-10 19:39:09 -0300

    fix(frontend): arrumando e validando login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,47 @@
 import { Link } from "react-router-dom";
+import Avatar from "./Avatar";
+import { useState } from "react";
+import Profile from "./Profile";
 
-const Header = () => {
+const Header = ({ user, setUser }) => {
+
+  const [openProfile, setOpenProfile] = useState(false)
+
   return (
     <header className="w-full p-2 text-white bg-slate-800">
-      <nav className="flex justify-around">
+      <nav className="flex justify-around items-center">
         <Link to="/" className="uppercase text-lg">Home</Link>
 
-        <ul className="flex space-x-8">
+        <ul className="flex space-x-8 items-center">
           <li className="list-none uppercase text-lg">
             <Link to="/form">Health form</Link>
           </li>
-          <li className="list-none uppercase text-lg">
-            <Link
-                className="text-white px-5 border-2 border-white rounded-lg bg-transparent cursor-pointer hover:bg-white hover:text-black transition-all duration-300" 
+          <li className="list-none uppercase text-lg relative">
+            {!user && (
+              <Link
+                className="text-white px-5 border-2 border-white rounded-lg bg-transparent cursor-pointer hover:bg-white hover:text-black transition-all duration-300"
                 to="/login"
-            >
+              >
                 Login
-            </Link>
+              </Link>
+            )}
+
+            {user && (
+              <Avatar 
+                user={user}
+                openProfile={openProfile}
+                setOpenProfile={setOpenProfile}
+              />
+            )}
+
+            {user && openProfile ? (
+            <Profile
+              user={user}
+              setUser={setUser}
+            />
+          ) : (null)}
           </li>
+          
         </ul>
       </nav>
     </header>
