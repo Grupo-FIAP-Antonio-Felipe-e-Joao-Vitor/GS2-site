28c01b4ea6795d58dc099a6a131fe10599e3c541
Antonio <antoniojacintoa@gmail.com>
2025-11-12 10:10:46 -0300

    fix(frontend): arrumando healthform

--- frontend/src/routes/HealthForm.jsx
+++ frontend/src/routes/HealthForm.jsx
@@ -18,7 +18,7 @@ const HealthForm = ({ user }) => {
       alert("Registre-se primeiro.")
       navigate("/login")
     }
-  })
+  }, [user, navigate])
 
   async function onSubmit(data) {
 
@@ -52,7 +52,7 @@ const HealthForm = ({ user }) => {
   return (
     <div className="flex flex-col py-5 w-full min-h-screen bg-slate-900">
 
-      <h1 className="text-4xl text-white uppercase font-bold text-center">Olá {user.nome}</h1>
+      <h1 className="text-4xl text-white uppercase font-bold text-center">Olá {user?.nome}</h1>
 
       <form
         className="w-full p-5 flex flex-col gap-2 items-center"
