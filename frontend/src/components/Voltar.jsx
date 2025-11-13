2949fbf62dae72453b51cb832b562cd5918b8f27
Antonio <antoniojacintoa@gmail.com>
2025-11-13 13:35:46 -0300

    feat(frontend): criando componente voltar ao dashboard

---
+++ frontend/src/components/Voltar.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom"
+
+const Voltar = () => {
+
+    const navigate = useNavigate()
+
+  return (
+
+    <div className="flex p-2 gap-2">
+
+        <span
+            onClick={() => navigate("/")}
+            className="text-gray-400 fixed top-20 left-10 text-lg font-semibold cursor-pointer hover:underline transition-all duration-300"
+        >
+            &larr; Voltar ao dashboard
+        </span>
+
+    </div>
+
+  )
+}
+
+export default Voltar
