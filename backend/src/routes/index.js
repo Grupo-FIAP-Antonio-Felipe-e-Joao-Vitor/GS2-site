191cf8dd2e7f4fbc1ade06b027b8fabe502fa308
Antonio <antoniojacintoa@gmail.com>
2025-11-13 15:24:43 -0300

    feat(backend): indexando as rotas do sensor

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import users from "./userRoutes.js";
 import analises from "./analiseRoute.js";
+import sensor from "./roomSensorRoute.js";
 import cors from "cors";
 
 const routes = (app) => {
-    app.use(cors(), express.json(), users, analises);
+    app.use(cors(), express.json(), users, analises, sensor);
 };
 
 export default routes;
