7f5c8b565a8a180fe47a9399ef16be8624b240ee
Felipe <febicaletto@gmail.com>
2025-11-14 10:29:41 -0300

    fix(frontend): arrumando component card form

--- frontend/src/components/CardForms.jsx
+++ frontend/src/components/CardForms.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 
-const CardForms = ({ titulo, descricao, data, form }) => {
+const CardForms = ({ titulo, descricao, data, form, realizado }) => {
   return (
     <div className="flex flex-col mt-15 w-60 min-h-64 h-full rounded-3xl">
       <div className="flex justify-center items-center bg-emerald-800 w-full rounded-t-2xl h-16">
@@ -11,9 +11,15 @@ const CardForms = ({ titulo, descricao, data, form }) => {
         <p className="text-lg">{descricao}</p>
         <p className="border-t mt-4">Prazo de finalização: <span className="font-semibold">{data}</span></p>
         <div className="flex justify-center mt-3">
-          <Link to={form} className="flex text-white bg-emerald-700 transition-all duration-300 hover:bg-emerald-800 w-fit px-5 h-8 justify-center items-center rounded-2xl font-medium">
-            Responder agora
-          </Link>
+          {realizado ? (
+            <button disabled className="flex text-white bg-gray-500 transition-all duration-300 hover:bg-emerald-800 w-fit px-5 h-8 justify-center items-center rounded-2xl font-medium cursor-not-allowed">
+              Já respondido
+            </button>
+          ):(
+            <Link to={form} className="flex text-white bg-emerald-700 transition-all duration-300 hover:bg-emerald-800 w-fit px-5 h-8 justify-center items-center rounded-2xl font-medium cursor-pointer">
+              Responder agora
+            </Link>
+          )}
         </div>
       </div>
 
