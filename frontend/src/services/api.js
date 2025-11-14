e596cf5f3244629a2fe722ab9c504ff729b0478f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-11-14 10:37:25 -0300

    fix(frontend): arrumando url da api no front

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+// URL localhost do backend
+const URL_local = "http://localhost:3000"
+
+// URL hospedado do backend
+const URL_render = "https://gs2-site.onrender.com"
+
 // Cria instância do axios
 const api = axios.create({
-    baseURL: "https://gs2-site.onrender.com" // URL base
+    baseURL: URL_render // URL base
 });
 
 export default api
